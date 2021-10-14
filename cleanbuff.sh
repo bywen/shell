fa4b8ac4a77bf0b04db9129681704bf2bcc402f7
bywen <48802901+bywen@users.noreply.github.com>
2021-10-14 15:14:38 +0800

    Update cleanbuff.sh

--- cleanbuff.sh
+++ cleanbuff.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
-sync;echo 1 > /proc/sys/vm/drop_caches
-#sync;echo 3 > /proc/sys/vm/drop_caches
+n=`free -h |grep Mem |awk '{print $4}' |awk -F . '{print $1}'`
 
+if [ $n -lt 2 ]
+then
+  sync;sync;sync #写入硬盘，防止数据丢失 
+  echo 1 > /proc/sys/vm/drop_caches  #清除page cache
+  echo 2 > /proc/sys/vm/drop_caches  #清除回收slab分配器中的对象(包括目录项缓存和inode缓存)
+  echo 3 > /proc/sys/vm/drop_caches  #清除pagecache和slab分配器中的缓存对象。
+fi
+exit
