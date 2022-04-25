bfa9e9be7cd83ffe8943f6a484833b050e9e395c
wen <info@bbddp.com>
2022-04-25 16:37:45 +0800

    update

--- backup_sql.sh
+++ backup_sql.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
-user="backup"
-passwd="17Dgap@Aw"
+user="DBbackup"
+passwd="30ffdadfb"
 dump="/usr/local/mysql/bin/mysqldump"
-dir="/backup/db_sql/"
-datetype=`date +%w`
+week=`date +%w`
+date=`date +%Y%m%d`
+dir="/backup/db_sql/file"
+chmod 755 $dir/*
+rm -f $dir/*.sql.gz
+
 for dbname in `cat /usr/local/sbin/db.txt`
 do
-	$dump -u$user -p$passwd --routines --events --triggers --single-transaction --flush-logs --databases $dbname | gzip > $dir/$dbname-$datetype.sql.gz
+  #过滤表 wr_sys_area
+  if [ $dbname == "snacksv2" ]
+  then
+	$dump -u$user -p$passwd -h localhost --routines --events --triggers --single-transaction --flush-logs --databases $dbname --ignore-table=snacksv2.wr_sys_area | gzip > $dir/$dbname-$date.sql.gz
+  else
+	$dump -u$user -p$passwd -h localhost --routines --events --triggers --single-transaction --flush-logs --databases $dbname | gzip > $dir/$dbname-$date.sql.gz
+  fi
 done
+/usr/bin/tar zcfP /backup/db_sql/snacks_$week.tar.gz -C $dir .
 
-cd $dir
-tar zcf /backup/send_file/zxz-$datetype.tar.gz /backup/db_sql/*
-/usr/bin/rsync -arvz /backup/send_file/zxz-$datetype.tar.gz wen@192.168.50.60::data/backup_mysql_zxz/ --password-file=/etc/rsync.password     
-exit
+#传到内网服务器
+/usr/bin/rsync -az /backup/db_sql/snacks_$week.tar.gz wen@11.131.13.31::data/snacks_backup/snacks_$date.tar.gz --password-file=/etc/rsync.password
